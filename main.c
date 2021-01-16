0fdcd543ade974da1a9f5c42d2a18de68ee52844
Youssef Lagtab <yousseflagtab@gmail.com>
2021-01-16 16:41:25 +0100

    set g_is_multi_dir_mode before listing
    previous behavior:
    - g_is_multi_dir_mode was set after freeing files and errors queues so
      their lenght will become 0.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 10:54:42 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/01/09 18:24:55 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/01/16 16:40:36 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,6 @@ static void	ft_ls(t_args *ls_args)
 	ft_free_queue(&(ls_args->files));
 	if (ls_args->files.length && ls_args->dirs.length)
 		ft_putstr("\n");
-	g_is_multi_dir_mode = ls_args->dirs.length > 1 || ls_args->files.length
-		|| ls_args->errors.length;
 	sort_list(&ls_args->dirs, get_cmp_func());
 	list_directories(&ls_args->dirs);
 	ft_free_queue(&(ls_args->dirs));
@@ -38,6 +36,8 @@ int			main(int ac, char **av)
 	(void)ac;
 	ft_bzero(&ls_args, sizeof(ls_args));
 	ft_parse(&ls_args, av + 1);
+	g_is_multi_dir_mode = ls_args.dirs.length > 1 || ls_args.files.length
+		|| ls_args.errors.length;
 	ft_ls(&ls_args);
 	ft_flush_buff();
 	return (errno > 0 ? 1 : 0);
